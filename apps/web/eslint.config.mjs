b9230683db70b8c04a4d49165763f3f533699b5b
Ana Popovic <franjetina@gmail.com>
2026-09-18 13:31:15 +0200

    fix(#5): fix failed tests

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: [".next/**", "dist/**", "eslint.config.mjs"],
+    ignores: [".next/**", "dist/**", "eslint.config.mjs", "next-env.d.ts"],
   },
   js.configs.recommended,
   {
